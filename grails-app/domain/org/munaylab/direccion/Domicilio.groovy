3303d227b31456621e6e816167a8ca072b81faf5
Augusto Caligares <mcaligares@gmail.com>
2018-06-02 21:19:25 -0300

    cambios de validacion de domicilio

--- grails-app/domain/org/munaylab/direccion/Domicilio.groovy
+++ grails-app/domain/org/munaylab/direccion/Domicilio.groovy
@@ -15,15 +15,15 @@ class Domicilio {
     String longitudPos
 
     static constraints = {
-        calle size: 3..50
+        calle size: 3..100
         numero size: 1..5
         piso nullable: true
         departamento nullable: true
-        barrio size: 3..50
-        distrito nullable: true, size: 3..50
-        localidad size: 3..50
-        provincia size: 3..50
-        pais nullable: true, size: 3..50
+        barrio size: 3..100
+        distrito nullable: true, size: 3..100
+        localidad size: 3..100
+        provincia size: 3..100
+        pais nullable: true, size: 3..100
         latitudPos nullable: true
         longitudPos nullable: true
     }
